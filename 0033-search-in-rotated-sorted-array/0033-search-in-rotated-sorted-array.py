125693a778c1a93134bc55bb9c59ff1203185cb1
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-03-03 11:13:15 +0500

    Time: 0 ms (100%), Space: 19.3 MB (88.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,6 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i]==target:
-                return i
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+
+            if nums[mid]==target:
+                return mid
+
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target and target < nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+
+            else:
+                if nums[mid]< target and target <= nums[right]:
+                    left= mid+1
+                else:
+                    right= mid-1
         return -1
