794794fae4dc962d7473ab1e631b1ac8dda27cc0
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-03-04 17:27:09 +0500

    Time: 36 ms (53.09%), Space: 42.5 MB (77.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,5 +1,5 @@
 class Solution:
-    def find_middle(self, head):
+    def find_midle(slef, head):
         fast = slow = head
         while fast and fast.next:
             fast = fast.next.next
@@ -9,19 +9,20 @@ class Solution:
         prev = None
         curr = head
         while curr:
-            next_ = curr.next
+            next_= curr.next
             curr.next = prev
             prev = curr
             curr = next_
         return prev
     def isSame(self, h1, h2):
         while h2:
-            if h1.val != h2.val:
+            if h1.val!=h2.val:
                 return False
-            h1 = h1.next
-            h2 = h2.next
+            h1= h1.next
+            h2= h2.next
         return True
     def isPalindrome(self, head):
-        middle = self.find_middle(head)
+        middle = self.find_midle( head)
         second = self.reverse(middle)
         return self.isSame(head, second)
+        
