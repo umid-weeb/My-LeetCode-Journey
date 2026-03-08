c011dd5a9e4c06cd5930c7ccc9f21588606fb7e2
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-03-08 21:06:53 +0500

    Time: 23 ms (51.77%), Space: 19.7 MB (71.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,7 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        count = [0] * 26
+
+        for c in magazine:
+            count[ord(c) - ord('a')] += 1
+
         for c in ransomNote:
-            if c not in magazine:
+            idx = ord(c) - ord('a')
+            count[idx] -= 1
+            if count[idx] < 0:
                 return False
-            magazine = magazine.replace(c,"",1)
+
         return True
