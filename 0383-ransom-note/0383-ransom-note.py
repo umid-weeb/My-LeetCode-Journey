50a04ac17dcd6d245936a7d0a84423d80dd4142c
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-03-08 21:03:24 +0500

    Time: 3 ms (96.96%), Space: 19.6 MB (93.14%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,7 @@
-from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        counter_magazine = Counter(magazine)
-        counter_ransom = Counter(ransomNote)
-
-        for i , n in counter_ransom.items():
-            if counter_magazine[i] <n:
+        for c in ransomNote:
+            if c not in magazine:
                 return False
+            magazine = magazine.replace(c,"",1)
         return True
