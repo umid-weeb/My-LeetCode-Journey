0c15b365944983105f67ac987f54e6741afbc83c
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-03-11 21:24:51 +0500

    Time: 83 ms (91.96%), Space: 28.4 MB (70.71%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,9 +3,9 @@ class Solution:
         n=len(nums)
         res = [0]*n
         stack =[]
-        for i in range(n):
-            while stack and nums[i] > nums[stack[-1]]:
-                prev_index = stack.pop()
-                res[prev_index]  = i - prev_index
+        for i , num in enumerate(nums):
+            while stack and nums[stack[-1]] < num:
+                index = stack.pop()
+                res[index]  = i - index
             stack.append(i)
         return res
