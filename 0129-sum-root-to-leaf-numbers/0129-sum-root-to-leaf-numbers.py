8af59c1e2d89fbac79d0dc16d1b68a180dab1815
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-05-10 12:09:49 +0500

    Time: 0 ms (100%), Space: 19.1 MB (99.18%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -4,13 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode], sum_=0) -> int:
-        if not root: return 0
-        sum_ = sum_*10+root.val
-
-
-        if not root.left and not root.right:
-            return sum_
-        return self.sumNumbers(root.left, sum_)+self.sumNumbers(root.right, sum_)
-        
+        if not root:
+            return 0
+        queue = deque([(root, root.val)])
+        while queue:
+            node, current_val = queue.popleft()
+            if not node.left and not node.right:
+                sum_ += current_val
+            if node.left:
+                queue.append((node.left, current_val * 10 + node.left.val))
+            if node.right:
+                queue.append((node.right, current_val * 10 + node.right.val))
+        return sum_
