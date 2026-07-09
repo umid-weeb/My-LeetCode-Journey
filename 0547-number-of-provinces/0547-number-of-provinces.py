f99a548db072e0af614b68d19105aeb37569d6ae
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-07-09 12:12:14 +0500

    Time: 7 ms (48.4%), Space: 21.6 MB (20.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -6,21 +6,18 @@ class Solution:
         count = 0
         visited = set()
         
-        for i  in range(n):
-            for j in range(n):
-                if isConnected[i][j]:
-                    graph[i].append(j)
-        def dfs(graph, i, visited):
-            if i  in visited: return
-            visited.add(i)
+    
+        def dfs(node):
+            for neighbor, connected in enumerate(isConnected[node]):
+                if connected and neighbor not in visited:
+                    visited.add(neighbor)
+                    dfs(neighbor)
 
-            for j in graph[i]:
-                if j not in visited:
-                    dfs(graph, j, visited)
 
         for i in range(n):
             if i not in visited:
-                dfs(graph, i, visited)
+                visited.add(i)
+                dfs(i)
                 count +=1
         return count
                 
