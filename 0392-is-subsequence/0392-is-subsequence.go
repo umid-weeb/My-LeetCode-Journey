6741a5a79290234f1b049192cd155a7fecb3ec06
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-08-27 20:52:51 +0500

    Time: 0 ms (100%), Space: 4 MB (58.85%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.go
@@ -0,0 +1,12 @@
+func isSubsequence(s string, t string) bool {
+    i :=0
+    j :=0
+    for i<len(s) && j<len(t){
+        if s[i]==t[j] {
+            i++
+        }
+        j++
+    }
+    return i==len(s)
+
+}
