6cf227a6a10a544ebc77b551b9ac1ff477617781
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-08-28 19:04:33 +0500

    Time: 7 ms (84.26%), Space: 19.7 MB (12.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,18 +1,13 @@
-
-def binary_search(tails, num):
-    low, high = 0, len(tails)
-    while low<high:
-        mid = (low+high)//2
-        if tails[mid]<num:
-            low = mid +1
-        else:
-            high = mid
-    return low
-
-
-
-
-
+import bisect
+# def binary_search(tails, num):
+#     low, high = 0, len(tails)
+#     while low<high:
+#         mid = (low+high)//2
+#         if tails[mid]<num:
+#             low = mid +1
+#         else:
+#             high = mid
+#     return low
 
 
 class Solution:
@@ -24,7 +19,8 @@ class Solution:
         perent = [-1]* len(nums)
 
         for i, num in enumerate(nums):
-            indx = binary_search(tails, num)
+            indx = bisect.bisect_left(tails, num)
+            # indx = binary_search(tails, num)
 
             if indx == len(tails):
                 tails.append(num)
