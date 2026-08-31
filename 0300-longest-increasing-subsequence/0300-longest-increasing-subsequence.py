9f4c9a2f6e58a44721f047d168ab7ece1098e200
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-08-31 19:58:10 +0500

    Time: 10 ms (80.05%), Space: 19.5 MB (58.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -12,7 +12,7 @@ def binary_search(tails, num):
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        temp : List[int] =[]
+        temp = list()
         for num in nums:
             idx = binary_search(temp, num)
             if idx<len(temp):
