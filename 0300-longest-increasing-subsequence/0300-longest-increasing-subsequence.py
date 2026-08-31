c9507260fd27dec3f59759a0496fd4d9d6b93d75
Isroilov Ibroximjon <isroilov0705@gmail.com>
2026-08-31 19:51:35 +0500

    Time: 7 ms (84.34%), Space: 19.6 MB (32.75%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -12,35 +12,13 @@ def binary_search(tails, num):
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return []
-        tails = []
-        tails_indices = []
-        perent = [-1]* len(nums)
-
-        for i, num in enumerate(nums):
-            # indx = bisect.bisect_left(tails, num)
-            indx = binary_search(tails, num)
-
-            if indx == len(tails):
-                tails.append(num)
-                tails_indices.append(i)
-
+        temp : List[int] =[]
+        for num in nums:
+            idx = binary_search(temp, num)
+            if idx<len(temp):
+                temp[idx] = num
             else:
-                tails[indx] = num
-                tails_indices[indx] =i
-
-
-
-            if indx >0:
-                perent[i] = tails_indices[indx -1]
-
-        result = []
-        current = tails_indices[-1]
-        while current != -1:
-            result.append(nums[current])
-            current = perent[current]
-        return len(result)
-
+                temp.append(num)
+        return len(temp)
 
         
